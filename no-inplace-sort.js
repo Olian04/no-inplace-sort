2dddf094f5805c65daa34d12503c51c43aa1e020
Oliver Anteros <rift95@live.se>
2018-08-21 13:22:46 +0200

    Update no-inplace-sort.js

--- no-inplace-sort.js
+++ no-inplace-sort.js
@@ -1,5 +1,5 @@
 Array.prototype.sort = (nativeInplaceSort => function(...args) {
   const arr = [...this]; // Shallow copy
   arr.sort = nativeInplaceSort; // Use native sort 
-	return [...arr.sort(...args)]; // Remove native sort
+  return [...arr.sort(...args)]; // Remove native sort
 })(Array.prototype.sort);
